8d7ea75c32f2ee7e717f894d415ebbe434bbe42f
C.R. Huerta Santiago <h.s.carlos.rodolfo@student.rug.nl>
2018-11-19 14:32:30 +0100

    Added randomly not conflicting selected integers

--- lab1-data/Solutions/Assignment2.m
+++ lab1-data/Solutions/Assignment2.m
@@ -1,9 +1,23 @@
 % Random Integer
 rInteger = randi([1 20]);
-disp(rInteger);
+fprintf('We are choosing the random %d file.\n', rInteger);
 
 % Use the sprintf function to generate the string
 fileToLoad = sprintf('person%02d.mat',rInteger); 
 
 % Load the data from the persons file
 data = load(fileToLoad);
+
+notReapeated = false;
+while notReapeated == false
+    % Choose randomly 2 rows
+    r1 = randi([1 20]);
+    r1 = randi([1 20]);
+    if r1 ~= r2
+        notReapeated = true;
+    else
+        notReapeated = false;
+    end
+end
+    
+fprintf('The first row index is: %d.\nThe second one is %d. \n',r1,r2);
