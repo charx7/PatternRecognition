af38ec4e8e15146c5565f29d5781020c9ec3d383
eserco <esercomak90@gmail.com>
2018-12-14 22:41:38 +0100

    Create assignment4.m

---
+++ lab4/assignment4.m
@@ -0,0 +1,34 @@
+clc;
+clear;
+
+%Store image in img variable 
+img = imread('HeadTool0002.bmp');
+
+%convert to double precision
+
+Y = double(J);
+
+fprintf('the type of array is: %s \n', class(Y));
+
+%apply CLAHE Method
+
+J = adapthisteq(img,'clipLimit',0.02,'Distribution','rayleigh');
+
+%compare enchanced and non-enhanced
+
+imshowpair(img, J,'montage');
+
+
+%find 6 circles
+[centers, radii, metric] = imfindcircles(J,[15 58]);
+
+%show 6 circles
+imshow(J);
+viscircles(centers, radii);
+
+%show two strongest circles
+imshow(J);
+centersStrong5 = centers(1:2,:); 
+radiiStrong5 = radii(1:2);
+viscircles(centersStrong5, radiiStrong5,'EdgeColor','b');
+
