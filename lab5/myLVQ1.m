5990a941fcc19b9e0237ccf7e435ac6c5c3f8929
eserco <esercomak90@gmail.com>
2018-12-29 22:52:11 +0100

    Update myLVQ1.m

--- lab5/myLVQ1.m
+++ lab5/myLVQ1.m
@@ -1,4 +1,4 @@
-function [Epochcount,epochsError,prototypeList,predictedLabels] = myLVQ1(dat,prototypes,class_labels,learningRate,plotMode,epochNumber)
+function [Epochcount,epochsError,prototypeList,predictedLabels] = myLVQ1(dat,prototypes,class_labels,learningRate,plotMode)
 %%
 %Returns: 
 %Epochcount - epoch count if the last parameter is not supplied.
@@ -6,11 +6,9 @@ function [Epochcount,epochsError,prototypeList,predictedLabels] = myLVQ1(dat,pro
 %prototypeList - updated prototype coordinate points
 %predictedLabels - new class labels for each data point
 
-%If the last parameter 'epochNumber' is supplied by the user, the function
-%stops when epoch number is reached by the for loop. Else, the function try
-%to converge error rate. Variable 'epochNumber' prevents below function to
-%escape for infinite loop and should be supplied when prototype count is
-%too low compared to the class count
+%If the parameter 'class_labels' is supplied by the user, the function
+%trains on the data supplied by 'dat' variable. Otherwise, the function
+%makes prediction.
 
 %Furthermore, the function has two plotting mode. Variable 'plotMode' set
 %to 1 outputs scatter plot of prototypes overlaid on the data. when this
@@ -22,11 +20,13 @@ function [Epochcount,epochsError,prototypeList,predictedLabels] = myLVQ1(dat,pro
 szData = size(dat);
 numberOfExamples = szData(1);
 epochsError = [];
-errorRatio = 1;
+errorRatio = 2;
 Epochcount = 0;
+predictedLabels = [];
+prototypeList = [];
 %%
-if ~exist('epochNumber','var') || isempty(epochNumber)
-    while (errorRatio<0.99900) || (errorRatio>0.99999)
+if exist('class_labels','Var')
+    while (errorRatio<0.99990) || (errorRatio>=1)
         currentEpochError = 0;
         % Do the loop for the number of numberOfExamples
         for i = 1:numberOfExamples
@@ -59,80 +59,51 @@ if ~exist('epochNumber','var') || isempty(epochNumber)
     
         %calculate whether t approaches constant
         if size(epochsError,1) < 2
-            errorRate = 1;
+            errorRatio = 2;
         else
             lError = size(epochsError,1);
             errorRatio = mean(epochsError(floor(lError*0.7):floor(lError*0.85)))/mean(epochsError(floor(lError*0.85):lError));
         end
         
         Epochcount = Epochcount + 1;
+        prototypeList = prototypes;
 
     end
-else
-    for k=1:epochNumber
-    currentEpochError = 0;
-        % Do the loop for the number of numberOfExamples
-        for i = 1:numberOfExamples
-            % Get the current Point
-            currentPoint = dat(i,:);
-            % Calculate the distances
-            currentDistance = pdist2(currentPoint, prototypes(:,1:2),'euclidean');
-            % Square the dist measure
-            currentDistanceSquare = currentDistance.^2;
-            % Calculate the min
-            [currentMin, minIdx] = min(currentDistanceSquare);
+end
+if ~exist('class_labels','Var') || isempty(class_labels)
+    %initiate list of zeros for predictedLabels
+    predicted = zeros(numberOfExamples,1);
 
-            % Update the positions of the prototypes based on the winner 
-            % Get the winner closer
-            winnerLabel = prototypes(minIdx,3);
-            currentLabel = class_labels(i);
-            if winnerLabel == currentLabel
-                % Move closer the winning prototype
-                prototypes(minIdx,1:2) = prototypes(minIdx,1:2) + learningRate * (currentPoint - prototypes(minIdx,1:2));
-            else
-                currentEpochError = currentEpochError + 1;
-                % Push away the winning prototype
-                prototypes(minIdx,1:2) = prototypes(minIdx,1:2) - learningRate * (currentPoint - prototypes(minIdx,1:2));
-            end
-            %prototypes([1:minIdx-1 minIdx+1:end],:) = prototypes([1:minIdx-1 minIdx+1:end],:) - learningRate*repmat(currentPoint,2,1);
+    %predict new labels
+    for f =1:numberOfExamples
+        currentPoint = dat(f,:);
+        % Calculate the distances
+        currentDistance = pdist2(currentPoint, prototypes(:,1:2),'euclidean');
+        % Square the dist measure
+        currentDistanceSquare = currentDistance.^2;
+        % Calculate the min
+        [currentMin, minIdx] = min(currentDistanceSquare);
+        %store predicted labels
+        predicted(f,1) = prototypes(minIdx,3);
+    end
+    
+    predictedLabels = predicted;
+end
+
+if exist('plotMode','Var')
+    if plotMode == 1
+        % Plot of the prototypes on top of the scatter
+        for p=1:size(prototypes,1)
+            plot(prototypes(p,1), prototypes(p,2), '*','MarkerSize',10);
+            hold on
         end
-        % Add to the epochs error matrix
-        currentErrorRate = currentEpochError / numberOfExamples;
-        epochsError = [epochsError; currentErrorRate];
-    end
-end
 
-prototypeList = prototypes;
-
-%initiate list of zeros for predictedLabels
-predictedLabels = zeros(200,1);
-
-%predict new labels
-for f =1:numberOfExamples
-    currentPoint = dat(f,:);
-    % Calculate the distances
-    currentDistance = pdist2(currentPoint, prototypeList(:,1:2),'euclidean');
-    % Square the dist measure
-    currentDistanceSquare = currentDistance.^2;
-    % Calculate the min
-    [currentMin, minIdx] = min(currentDistanceSquare);
-    %store predicted labels
-    predictedLabels(f,1) = prototypeList(minIdx,3);
-end
-    
-if plotMode == 1
-    % Plot of the prototypes on top of the scatter
-    for p=1:size(prototypes,1)
-        plot(prototypes(p,1), prototypes(p,2), '*','MarkerSize',10);
+    elseif plotMode == 2
+        % Plot the number of epochs vs error
+        plot(epochsError);
+        legend('Error')
         hold on
+
     end
-    
-elseif plotMode == 2
-    % Plot the number of epochs vs error
-    plot(epochsError);
-    legend('Error')
-    hold on
-    
-elseif plotMode == 3
 end
 end
