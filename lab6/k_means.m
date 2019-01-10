7ef7356337177044c6a2a53c4689582ffabde4a0
C.R. Huerta Santiago <h.s.carlos.rodolfo@student.rug.nl>
2019-01-10 14:21:27 +0100

    finished k means with basic coloring :D

--- lab6/k_means.m
+++ lab6/k_means.m
@@ -13,6 +13,8 @@ szData = size(dataMat);
 numberOfExamples = szData(1);
 k = 2;
 centroids = [];
+clusterLabels = zeros(numberOfExamples,1);
+maxEpochs = 1000;
 
 % take random samples from the data set
 for i=1:k
@@ -22,3 +24,55 @@ for i=1:k
     centroids = [centroids; randomPoint];
 end
 
+% Epochs loop
+for j=1:maxEpochs
+    % Assign data points to the nearest cluster
+    for i=1:numberOfExamples
+        % Get the current Point
+        currentPoint = dataMat(i,:);
+        % Calculate the distances
+        currentDistance = pdist2(currentPoint, centroids(:,1:2),'euclidean');
+        % Calculate the min
+        [currentMin, minIdx] = min(currentDistance);
+        % Assign the cluster to the min distance
+        clusterLabels(i,1) = minIdx;
+    end
+
+    % Re compute the centroids as the mean of the points assigned to each
+    % cluster
+    % Old centroids as a stop condition for algo
+    oldCentroids = centroids;
+    for i=1:k
+        % Find the indexes where we have the current cluster assign
+        currentCentroidIndexes = find(clusterLabels == i);
+        % Get the points
+        currentCentroidPoints = dataMat(currentCentroidIndexes,:);
+        % Recompute the centroid as the mean of the assigned points :D
+        newCentroid = mean(currentCentroidPoints);
+        % Re-assign the current centroid as the mean
+        centroids(i,:) = newCentroid;
+    end
+    
+    if abs(norm(oldCentroids) - norm(centroids))< 0.01
+        % Escape the loop
+        break
+    end
+end
+
+colorz = ['r','g','b','c'];
+% Plotz for dayz
+for p=1:size(centroids,1)
+    plot(centroids(p,1), centroids(p,2), '-o','MarkerSize',10, 'MarkerFaceColor', colorz(p));
+    hold on
+end
+% Scatter Plot of the dataz
+scatterData = [[dataMat] clusterLabels];
+x1 = scatterData(:,1);
+x2 = scatterData(:, 2);
+classes = scatterData(:,3);
+scatter(x1(classes == 1), x2(classes == 1))
+hold on;
+scatter(x1(classes == 2), x2(classes == 2))
+hold off
+legend('First Cluster','Second Cluster','Cluster 1','Cluster 2')
+title('Scatter Plot of k=2');
